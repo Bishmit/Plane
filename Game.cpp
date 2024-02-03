aee61ea41c13fa1dbcdac99c5a71771fb4c99860
Bishmit Regmi <bishmitregmi@gmail.com>
2024-02-03 13:37:10 +0545

    title changes

--- Game.cpp
+++ Game.cpp
@@ -28,7 +28,7 @@ void Game::initVariable() {
 
 void Game::initWindow() {
     videomode = sf::VideoMode(800, 800);
-    window = new sf::RenderWindow(videomode, "Desert War", sf::Style::Close | sf::Style::Titlebar);
+    window = new sf::RenderWindow(videomode, "Plane", sf::Style::Close | sf::Style::Titlebar);
     backgroundtexture.loadFromFile("Desert.png"); 
     bgsprite.setTexture(backgroundtexture); 
     this->window->setFramerateLimit(60);
